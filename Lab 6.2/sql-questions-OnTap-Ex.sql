99b51eb8593eade8d08875e64233847e7ef13c45
leehugw <hungnguyenle1307@gmail.com>
2024-12-29 11:22:21 +0700

    Update sql-questions-OnTap-Ex.sql

--- Lab 6.2/sql-questions-OnTap-Ex.sql
+++ Lab 6.2/sql-questions-OnTap-Ex.sql
@@ -2,7 +2,7 @@
 -- 23520567
 -- IT003.P113.2 - Lab06
 
--- Link github: 
+-- Link github: https://github.com/leehugw/IT004.P113/blob/main/Lab%206.2/sql-questions-OnTap-Ex.sql
 
 -- Câu hỏi SQL từ cơ bản đến nâng cao, bao gồm trigger
 
